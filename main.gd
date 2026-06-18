f6dbcd35d27fb0fedc23c92e7766ed5af29731b1
ARNAV AGGARWAL <h2510013@nushigh.edu.sg>
2026-06-19 07:08:16 +0800

    added crunch sound

--- main.gd
+++ main.gd
@@ -1,17 +1,18 @@
 extends Node
 
-var dragging :=false
-var drag_offset :=Vector2i.ZERO
+@onready var crunch_sound = $CrunchSound
+var dragging := false
+var drag_offset := Vector2i.ZERO
 
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.button_index ==MOUSE_BUTTON_LEFT:
+		if event.button_index == MOUSE_BUTTON_LEFT:
 			dragging = event.pressed
 			drag_offset = DisplayServer.window_get_position() - DisplayServer.mouse_get_position()
+		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+			crunch_sound.play()
+	
 	if event is InputEventMouseMotion and dragging:
 		DisplayServer.window_set_position(
 			DisplayServer.mouse_get_position() + drag_offset
 		)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
